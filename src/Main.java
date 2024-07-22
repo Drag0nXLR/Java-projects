866601593262ca2688fa4e8605f620215fa806fc
Drag0nXLR <kovalyov17vova@gmail.com>
2024-07-22 19:56:54 +0300

    Started project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//Started project
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args) {
+        String text;
+
+        text = JOptionPane.showInputDialog("Enter text");
+        JOptionPane.showMessageDialog(null, "You inputed the next text:\n " + text);
+    } //End of description of method
+} //End of description of class
